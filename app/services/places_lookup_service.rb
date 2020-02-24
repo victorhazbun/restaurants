6619c0c8e1faf690fbc3e10a6d40f0ef82df7b61
victor hazbun <victorhazbun87@gmail.com>
2020-02-23 20:10:26 -0500

    places lookup service comments

--- app/services/places_lookup_service.rb
+++ app/services/places_lookup_service.rb
@@ -1,4 +1,11 @@
 class PlacesLookupService
+
+  # NOTE: Ideally the result should be cached.
+  # That way we don't make unessesary API calls.
+  #
+  # NOTE: Ideally yhis class should accept any lookup client.
+  # That way we can switch beteen clients without touching this class.
+  # The adapter design pattern should work to achive that behaivour.
   def self.call(lookup_client:, lat:, lng:, types:)
     lookup_client.spots(lat, lng, types)
   end
