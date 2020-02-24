e3d691aaa46a48f616ea3a5369992c42c958a00b
victor hazbun <victorhazbun87@gmail.com>
2020-02-23 21:14:14 -0500

    Cache palces lookup result

--- app/services/places_lookup_service.rb
+++ app/services/places_lookup_service.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 class PlacesLookupService
-  # NOTE: Ideally the result should be cached.
-  # That way we don't make unessesary API calls.
-  #
-  # NOTE: Ideally yhis class should accept any lookup client.
+  # NOTE: Ideally This class should accept any lookup client.
   # That way we can switch beteen clients without touching this class.
   # The adapter design pattern should work to achive that behaivour.
   def self.call(lookup_client:, lat:, lng:, types:, &callback)
-    result = lookup_client.spots(lat, lng, types)
+    result = Rails.cache.fetch("lat-#{lat}-lng-#{lng}-types-#{types}", expires_in: 24.hours) do
+      lookup_client.spots(lat, lng, types: types)
+    end
     callback.call
     result
   end
